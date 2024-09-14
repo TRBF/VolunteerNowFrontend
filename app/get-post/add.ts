4707dcdc567339efa047bc3a7271980f49a8a518
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-09-14 17:48:57 +0300

    addExperiences

--- app/get-post/add.ts
+++ app/get-post/add.ts
@@ -1,6 +1,6 @@
 // functie pt login la users
 export async function getUsers() {
-    const url = 'http://localhost:3000/api/login?username&password';
+    const url = 'http://188.214.88.101:3000/api/login?username&password';
     const response = await fetch(url);
     const users = await response.json();
     return users;
@@ -8,7 +8,7 @@ export async function getUsers() {
 
 // functie pt creare de user:
 export async function createUser(username, password, gender, first_name, last_name, email) {
-    const url = 'http://localhost:3000/api/register';
+    const url = 'http://188.214.88.101:3000/api/register';
     const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -21,14 +21,14 @@ export async function createUser(username, password, gender, first_name, last_na
 }
 
 export async function eventGet(number) {
-    const url = 'http://localhost:3000/api/get_events?page';
+    const url = 'http://188.214.88.101:3000/api/get_events?page';
     const response = await fetch(url);
     const event = await response.json();
     return event;
 }
 
 export async function eventAdd(name, description, link_to_pfp, link_to_cover_image, edition, location, time) {
-    const url = 'http://localhost:3000/api/add_event';
+    const url = 'http://188.214.88.101:3000/api/add_event';
     const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -38,4 +38,12 @@ export async function eventAdd(name, description, link_to_pfp, link_to_cover_ima
     });
     const newEvent = await response.json();
     return newEvent;   
+}
+
+export async function addExperiences(userid){
+    const url = `http://188.214.88.101:3000/api/get_experiences/${userid}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (!result.success)return [];
+    return result.result;
 }
